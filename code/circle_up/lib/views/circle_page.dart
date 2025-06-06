a6b6203ae6b71755ae025b1f2b100cae0573f25f
EmilyMeng0501 <xmeng05@uw.edu>
2025-06-06 09:58:26 -0700

    circle page

--- code/circle_up/lib/views/circle_page.dart
+++ code/circle_up/lib/views/circle_page.dart
@@ -7,19 +7,32 @@ import '../services/user_service.dart';
 import 'photo_gallery.dart';
 import '../models/user.dart';
 
+/// This class will display details about the current alarm circle, 
+/// including the group code, alarm time, members, leave group
+/// and upload/view photos 
 class CirclePage extends StatefulWidget {
   final AlarmCircle circle;
+  /// the constructor for the class
+  /// Parameters: 
+  /// - circle: the circle group
   const CirclePage({super.key, required this.circle});
 
+  /// creates the mutable state for this page
   @override
   State<CirclePage> createState() => _CirclePageState();
 }
 
+/// This class handles fetching group members, formatting the display times. 
+/// and respond to user actions like upload their morning photo or leave the group
 class _CirclePageState extends State<CirclePage> {
+  /// interacting with alarm circle data
   final AlarmCircleService _circleService = AlarmCircleService();
+  /// interacting with user data
   final UserService _userService = UserService();
+  /// loads member user data for the current circle group
   late Future<List<AppUser>> _membersFuture;
 
+  /// Takes in a DateTime object and formats it to a 12 hour format as a string
   String _formatDateTime(DateTime dateTime) {
     final hour =
         dateTime.hour == 0
@@ -31,6 +44,9 @@ class _CirclePageState extends State<CirclePage> {
     return '${hour.toString().padLeft(2, '0')}:${dateTime.minute.toString().padLeft(2, '0')} $period';
   }
 
+  /// Leaves the current circle and navigates to the no group page
+  /// If the user is successfully removed from the circle, we navigate to the no group page
+  /// If there is an error, display an error message in the snackbar
   Future<void> _leaveCircle(BuildContext context) async {
     try {
       await _circleService.leaveCircle(widget.circle.id);
@@ -47,12 +63,18 @@ class _CirclePageState extends State<CirclePage> {
     }
   }
 
+  // Initializes the state and fetches all of the members in the circle
+  // Gets a list of all of the members in the circle, by their ids
   @override
   void initState() {
     super.initState();
     _membersFuture = _userService.getUsersByIds(widget.circle.memberIds);
   }
 
+  /// Builds the Circle Page UI
+  /// Returns: 
+  /// - a scaffold widget with the described circle widget displays
+  /// We made this function shorter by creating some helper methods down below
   @override
   Widget build(BuildContext context) {
     final circle = widget.circle;
@@ -85,9 +107,13 @@ class _CirclePageState extends State<CirclePage> {
     );
   }
 
+  /// Contains buttons to either upload a photo or a button to view the gallery of photos in the circle
+  /// Returns: the AppBar for the Circle Page
+  /// This method also contains semantic labels for accessibility sake
   AppBar _buildAppBar(AlarmCircle circle) {
     return AppBar(
       actions: [
+        // On click, taken to the upload photo page
         Semantics(
           label: 'Upload Photo',
           button: true,
@@ -99,6 +125,7 @@ class _CirclePageState extends State<CirclePage> {
             },
           ),
         ),
+        // On click, taken to the photo gallery page
         Semantics(
           label: 'View Photos',
           button: true,
@@ -132,6 +159,9 @@ class _CirclePageState extends State<CirclePage> {
     );
   }
 
+  /// Reusable across the page for various titles
+  /// Returns: 
+  /// - a semantic label that builds a header for a section with the given title
   Widget _buildSectionHeader(String title) {
     return Semantics(
       header: true,
@@ -142,6 +172,10 @@ class _CirclePageState extends State<CirclePage> {
     );
   }
 
+  /// Builds the Circle code box representing the circle code
+  /// Further, contains a button to copy the circle code to the clipboard, so can be sent to others
+  /// Returns: 
+  /// - a semantic label that contains the widget described above
   Widget _buildCircleCodeBox(AlarmCircle circle) {
     return Semantics(
       label: 'Circle code is ${circle.code}',
@@ -151,6 +185,7 @@ class _CirclePageState extends State<CirclePage> {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
+            // Displays the circle code in a large text
             Semantics(
               label: 'Circle code text',
               child: Text(
@@ -162,6 +197,7 @@ class _CirclePageState extends State<CirclePage> {
                 ),
               ),
             ),
+            // Button to copy the circle code to the clipboard
             Semantics(
               label: 'Copy circle code',
               button: true,
@@ -183,6 +219,10 @@ class _CirclePageState extends State<CirclePage> {
     );
   }
 
+  /// Builds the widget that displays the alarm time for the circle
+  /// This represents the time that was set when the circle was created
+  /// Returns: 
+  /// - a semantic label that contains the widget described above
   Widget _buildAlarmTimeBox(AlarmCircle circle) {
     return Semantics(
       label: 'Alarm time is ${_formatDateTime(circle.alarmTime)}',
@@ -209,6 +249,10 @@ class _CirclePageState extends State<CirclePage> {
     );
   }
 
+  /// This widget builds all of the members in the circle
+  /// It displays all of the members names and their success/failure counts
+  /// Returns: 
+  /// - a semantic label that contains the widget described above
   Widget _buildMembersBox() {
     return Semantics(
       label: 'Circle members list',
@@ -239,6 +283,7 @@ class _CirclePageState extends State<CirclePage> {
                   ),
                 ),
                 const SizedBox(height: 10),
+                // List of members with their success and failure counts
                 ...members.map(
                   (user) => Semantics(
                     label:
@@ -260,13 +305,16 @@ class _CirclePageState extends State<CirclePage> {
     );
   }
 
+  /// Builds the widget that when clicked will allow for the user to leave the circle
+  /// Returns: 
+  /// - a Center label that contains the widget described above
   Widget _buildLeaveButton(BuildContext context) {
     return Center(
       child: Semantics(
         label: 'Leave Circle',
         button: true,
         child: ElevatedButton(
-          onPressed: () => _leaveCircle(context),
+          onPressed: () => _leaveCircle(context), // Leaves the circle on click
           style: ElevatedButton.styleFrom(
             backgroundColor: Colors.red[400],
             padding: const EdgeInsets.symmetric(
@@ -283,6 +331,10 @@ class _CirclePageState extends State<CirclePage> {
     );
   }
 
+  /// Builds a decoration for boxed used throughout the circle page
+  /// Provides a way to reuse the same decoration for various boxes
+  /// Returns: 
+  /// - a box decoration widget that builds views described as above
   BoxDecoration _boxDecoration() {
     return BoxDecoration(
       color: Colors.white,
